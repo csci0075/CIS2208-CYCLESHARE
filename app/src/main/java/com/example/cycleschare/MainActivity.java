5d80c4984767173f2d1c1a0622aae3643cbc8cd1
Clyde Sciberras <62725610+csci0075@users.noreply.github.com>
2023-05-17 18:28:02 +0200

    Added Greeting Based on Time of Day to Initial Page

--- app/src/main/java/com/example/cycleschare/MainActivity.java
+++ app/src/main/java/com/example/cycleschare/MainActivity.java
@@ -4,22 +4,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
+
 public class MainActivity extends AppCompatActivity {
+    TextView Greeting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Backend of 'Begin' button to go to next activity
-        Button btn = (Button) findViewById(R.id.begin_button);
+        //Backend of 'Begin' button to go to the next activity
+        Button btn = findViewById(R.id.begin_button);
+
+        Greeting = findViewById(R.id.Greeting);
+
+        // Update the greeting based on the current time
+        updateTimeBasedGreeting();
+
         btn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 startActivity(new Intent(MainActivity.this, AuthActivity.class));
             }
         });
+    }
+
+    private void updateTimeBasedGreeting() {
+        Calendar calendar = Calendar.getInstance();
+        int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+
+        if (hourOfDay >= 0 && hourOfDay < 12) {
+            Greeting.setText("Good Morning!");
+        } else if (hourOfDay >= 12 && hourOfDay < 18) {
+            Greeting.setText("Good Afternoon!");
+        } else {
+            Greeting.setText("Good Evening!");
+        }
     }
 }
