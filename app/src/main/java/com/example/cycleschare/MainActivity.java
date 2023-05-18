4ed195c1974d0b76fa9542034808108efba7c514
Clyde Sciberras <62725610+csci0075@users.noreply.github.com>
2023-05-18 18:57:40 +0200

    Added Good Morning / Afternoon / Evening Greeting
    After another attempt and a bit more research, I have managed to make the app show a good morning / afternoon / evening greeting. While this had been successful before, the app crashed when pressing begin. Now, the app does not crash and proceeds as usual.
    
    Therefore, the implementation was successful.

--- app/src/main/java/com/example/cycleschare/MainActivity.java
+++ app/src/main/java/com/example/cycleschare/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import java.util.Calendar; // in order to be able to make the greeting work
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView Greeting = findViewById(R.id.Greeting);
+
+        int currentTime = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+
+        if (currentTime >= 5 && currentTime < 12) {
+            Greeting.setText("Good Morning");
+        } else if (currentTime >= 12 && currentTime < 17) {
+            Greeting.setText("Good Afternoon");
+        } else if (currentTime >= 17 && currentTime < 21) {
+            Greeting.setText("Good Evening");
+        } else {
+            Greeting.setText("Good Night");
+        }
 
         //Backend of 'Begin' button to go to next activity
         Button btn = (Button) findViewById(R.id.begin_button);
